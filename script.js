4b80ca99c1ecb7fb29e7260bb3c0e5189eb1259b
yulopadua <yulo.padua@gmail.com>
2022-05-21 14:33:51 +0800

    Add operate function

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 //functions for basic math operators
 function add(a, b){
     return a + b;
-}
+};
 
 function subtract(a, b){
     return a - b;
-}
+};
 
 function multiply(a, b){
     return a * b;
-}
+};
 
 function divide(a, b){
     return a / b;
+};
+
+//function that takes an operator and 2 numbers then call one of the above functions
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b)
+    } else if (operator === "-") {
+        return subtract(a, b)
+    } else if (operator === "*") {
+        return multiply(a, b)
+    } else if (operator === "/") {
+        return divide(a, b)
+    }
 }
